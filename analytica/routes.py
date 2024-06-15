6ac9c4accf3c68b25be6bd92bf671b4feb669c96
ahmed09832 <ahmedshawaly70@gmail.com>
2024-06-16 01:11:18 +0300

    add register template and route

--- analytica/routes.py
+++ analytica/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash
 from analytica.forms import ProductCodeForm, RegisterForm, LoginForm
 from analytica.models import User
 
-from analytica import app
+from analytica import app, db
 
 
 
@@ -17,6 +17,27 @@ def home_page():
 @app.route("/about")
 def about_page():
     return render_template("about.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, 
+                              email_address=form.email_address.data, 
+                              password_hash = form.password1.data
+                              )
+        db.session.add(user_to_create)
+        db.session.commit()
+        flash('Account created successfully!', category='success')
+
+        return redirect(url_for('home_page'))
+    
+    if form.errors:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template("register.html", form=form)
 
 
 @app.route('/dashboard')
