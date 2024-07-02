a1bd4009162d2d52a1d4ba574e438a4c6642f752
ahmed09832 <ahmedshawaly70@gmail.com>
2024-07-02 17:26:04 +0300

    change the url validator in the form

--- analytica/forms.py
+++ analytica/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Email, Length, DataRequired, ValidationError, EqualTo
 from analytica.models import User
 
+from analytica.scraping import get_html_content, extract_asin
 class RegisterForm(FlaskForm):
     
     def validate_username(self, username_to_check):
@@ -36,26 +37,53 @@ class LoginForm(FlaskForm):
     submit = SubmitField(label='Sign In:')
 
 
+# def is_valid_url_or_code(form, field):
+#     input_value = field.data
+#     url_pattern = r'^https?://(?:www\.)?amazon\.[a-z]{2,3}(?:\.[a-z]{2})?/.*[/?]dp/([A-Z0-9]{10})(?:/.*)?$'
+#     code_pattern = r'^[A-Z0-9]{10}$'
+
+#     if re.match(code_pattern, input_value):
+#         url = f"https://www.amazon.com/dp/{input_value}"
+#     else:
+#         url = input_value
+
+#     print("---------------------------------->\n")
+#     print(not re.match(url_pattern, url))
+#     print("---------------------------------->\n")
+
+#     if not re.match(url_pattern, url):
+#         print("HI ------------------- HI")
+#         raise ValidationError('Invalid Amazon product URL or code format.')
+
+#     try:
+#         res = requests.get(url, timeout=10)
+#         if res.status_code != 200:
+#             raise ValidationError('The product URL is not accessible.')
+#     except requests.RequestException as e:
+#         raise ValidationError(f'Error accessing the URL: {str(e)}')
+
+
 def is_valid_url_or_code(form, field):
     input_value = field.data
     url_pattern = r'^https?://(?:www\.)?amazon\.[a-z]{2,3}(?:\.[a-z]{2})?/.*[/?]dp/([A-Z0-9]{10})(?:/.*)?$'
     code_pattern = r'^[A-Z0-9]{10}$'
+    
+    
 
-    if re.match(code_pattern, input_value):
-        url = f"https://www.amazon.com/dp/{input_value}"
-    else:
-        url = input_value
-
-    if not re.match(url_pattern, url):
-        raise ValidationError('Invalid Amazon product URL or code format.')
-
+    if not re.match(code_pattern, input_value):
+        product_code = extract_asin(input_value)
+        
+    else: 
+        product_code = input_value
+                
     try:
-        res = requests.get(url, timeout=10)
+        res = get_html_content(product_code, 1)
         if res.status_code != 200:
             raise ValidationError('The product URL is not accessible.')
     except requests.RequestException as e:
         raise ValidationError(f'Error accessing the URL: {str(e)}')
-
+    
+    
 class ProductCodeForm(FlaskForm):
     product_url_or_code = StringField('Product URL or Code', validators=[DataRequired(), is_valid_url_or_code])
     submit = SubmitField(label='Analysis')
